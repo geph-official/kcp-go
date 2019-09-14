4e10441a104cdc1c5a8e417f4ac7a3abc0990d6b
nullchinchilla <nullchinchilla@pm.me>
2019-09-14 14:58:23 -0400

    ha

--- kcp.go
+++ kcp.go
@@ -668,7 +668,7 @@ func (kcp *KCP) Input(data []byte, regular, ackNoDelay bool) int {
 			// update speed sample
 			speedSamp := float64(acks) / time.Since(kcp.DRE.lastAckTime).Seconds()
 			for i := 0; i < int(acks); i++ {
-				kcp.DRE.interval = 0.9*kcp.DRE.interval + 0.1*(1/speedSamp)
+				kcp.DRE.interval = 0.99*kcp.DRE.interval + 0.01*(1/speedSamp)
 			}
 			kcp.DRE.lastAckTime = time.Now()
 			log.Println("Speed estimate is", 1/kcp.DRE.interval, "pkt/s")
